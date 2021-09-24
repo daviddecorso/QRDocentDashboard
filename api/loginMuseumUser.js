064c79c27e204745eeb9a930f0367cf8a4587f8d
Daniel Sanchez <sancd22@gmail.com>
2021-09-24 02:48:38 -0400

    Twilio API workingclear

--- api/loginMuseumUser.js
+++ api/loginMuseumUser.js
@@ -20,16 +20,25 @@ module.exports = async(req, res) =>
 
     const twilioCredentials = require('../configuration').getTwilioCredentials();
     const client = require('twilio')(twilioCredentials.accountSID, twilioCredentials.authToken);
-
-    // Literally just doesn't work
+    
     client.messages 
     .create({ 
         body: 'hi :D',  
         messagingServiceSid: twilioCredentials.messagingServiceSID,
         to: '+13057101065' 
     }) 
-    .then(message => console.log(message.sid)) 
-    .done();
+    .then(message => {
+        console.log(message.sid);
+        res.status(200).send(JSON.stringify('done'));
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(200).send(JSON.stringify('error'));
+    });
+    
 
-    res.status(200).send(JSON.stringify('done'));
+    //process: generate a random num and send to phone num, then pass that random generate number 
+    // down as http response and have frontend verify if user inputs the same random num to confirm login
+
+    
 };
