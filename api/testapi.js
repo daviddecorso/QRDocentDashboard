d046901c769a42d3d7acae88c54b3dccc6b8eb11
Daniel Sanchez <sancd22@gmail.com>
2021-10-02 13:05:13 -0400

    argon2 hash for passwords

--- api/testapi.js
+++ api/testapi.js
@@ -1,12 +1,13 @@
-import generate from './utility/generateToken';
+import argon2 from './utility/argon2';
 
-module.exports = (req, res) => {
+module.exports = async(req, res) => {
     //const { name = 'default' } = req.query;
     const pass = "apple";
-    const hashedPass = generate.encrypt(pass);
+    const hashedPass = await argon2.hash(pass);
 
-    const pass2 = "apple2asdasdadsaasasdsaasdsadsadasds";
-    const hashedPassed2 = generate.encrypt(pass2);
-    const areTheyEqual = hashedPass === hashedPassed2;
-    res.status(200).send(`pass1: ${pass}\thashedPass1: ${hashedPass}\npass2: ${pass2}\thashedPass2: ${hashedPassed2}\n\nAre they equal?: ${areTheyEqual}\n\nLength of hashedPass1: ${hashedPass.length}\tLength of hashedPass2: ${hashedPassed2.length}`);
+    const pass2 = "applesdfsfdssfsdsdfdsdsfsdsdfdsfsfdsfdsfdsfdsdsfsfsdfdsdsf";
+    const hashedPassed2 = await argon2.hash(pass2);
+
+    const areTheyEqual = await argon2.verify(hashedPassed2, pass2);
+    res.status(200).send(`pass1: ${pass}\nhashedPass1: ${hashedPass}\n\npass2: ${pass2}\nhashedPass2: ${hashedPassed2}\n\nAre they equal?: ${areTheyEqual}\n\nLength of hashedPass1: ${hashedPass.length}\nLength of hashedPass2: ${hashedPassed2.length}`);
 };
