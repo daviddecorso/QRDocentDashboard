fd1b991a5ada40ac76b70bb33fafea6aa670cfe4
Daniel Sanchez <sancd22@gmail.com>
2021-11-01 23:38:31 -0400

    Spotify APIs

--- api/spotifyCallback.js
+++ api/spotifyCallback.js
@@ -1,39 +1,41 @@
-import { failure, success } from './utility/responseObject';
+import axios from 'axios';
+import { failure } from '../api/utility/responseObject';
 import { getBaseURL } from '../configuration';
-import authentication from './middleware/authentication';
-import request from 'request';
-import querystring from 'querystring';
 
 const API = async(req, res) =>
 {
-    // TODO: Fix naming, depracations, and make calls async/await!
-    const redirect_uri = getBaseURL() + 'api/spotifyCallback';
-    let code = req.query.code || null;
-    let authOptions = {
+    const redirectURI = getBaseURL() + 'api/spotifyCallback';
+    const code = req.query.code;
+    const configOptions = {
+        method: 'post',
         url: 'https://accounts.spotify.com/api/token',
-        form: {
+        params: {
             code: code,
-            redirect_uri,
+            redirectURI,
             grant_type: 'authorization_code'
         },
         headers: {
-            'Authorization': 'Basic ' + (new Buffer(
-                process.env.SPOTIFY_QR_DOCENT_ID + ':' + process.env.SPOTIFY_QR_DOCENT_SECRET
-            ).toString('base64'))
-        },
-        json: true
+            Authorization: 'Basic ' +
+                Buffer.from(process.env.SPOTIFY_QR_DOCENT_ID + ':' + process.env.SPOTIFY_QR_DOCENT_SECRET)
+                    .toString('base64')
+        }
     };
 
-    request.post(authOptions, function (error, response, body) {
-        var access_token = body.access_token
-        var refresh_token = body.refresh_token
-        console.log(body)
-        let uri = process.env.FRONTEND_URI || 'exp://192.168.1.155:19000'
-        res.redirect(uri + '?access_token=' + access_token + '&refresh_token=' + refresh_token)
-    })
+    try
+    {
+        const response = await axios(configOptions);
+        const accessToken = response.body.access_token;
+        const refreshToken = response.body.refresh_token;
+
+        console.log(response.body);
+        res.redirect(process.env.FRONTEND_URI + '?access_token=' + accessToken + '&refresh_token=' + refreshToken);
+    }
+    catch (error)
+    {
+        console.log(error);
+        res.status(200).setHeader('Content-Type', 'application/json')
+            .send(JSON.stringify(failure(error.message)));
+    }
 };
 
 export default API;
-
-
-
--- api/spotifyLogin.js
+++ api/spotifyLogin.js
@@ -1,23 +1,17 @@
-import { failure, success } from './utility/responseObject';
 import { getBaseURL } from '../configuration';
-import query from '../database/databaseConnection';
-import querystring from 'querystring';
+import { URLSearchParams } from 'url';
 
-const API = async(req, res) =>
+const API = (req, res) =>
 {
-    // TODO: Fix naming, depracations, and make calls async/await!
-    const redirect_uri = getBaseURL() + 'api/spotifyCallback';
-    
-    res.redirect('https://accounts.spotify.com/authorize?' +
-        querystring.stringify({
-            response_type: 'code',
-            client_id: process.env.SPOTIFY_QR_DOCENT_ID,
-            scope: 'user-read-private user-read-email ugc-image-upload playlist-modify-public playlist-modify-private',
-            redirect_uri
-        }))
+    const redirectURI = getBaseURL() + 'api/spotifyCallback';
+    const queryString = new URLSearchParams({
+        response_type: 'code',
+        client_id: process.env.SPOTIFY_QR_DOCENT_ID,
+        scope: 'user-read-private user-read-email ugc-image-upload playlist-modify-public playlist-modify-private',
+        redirectURI
+    });
+
+    res.redirect('https://accounts.spotify.com/authorize?' + queryString);
 };
 
 export default API;
-
-
-
