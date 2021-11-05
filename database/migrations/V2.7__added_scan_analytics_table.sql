7422b1d5a51e723d6ad451eecc6c6f4377b2b58f
Daniel Sanchez <sancd22@gmail.com>
2021-11-05 18:39:58 -0400

    update .sql

--- database/migrations/V2.7__added_scan_analytics_table.sql
+++ database/migrations/V2.7__added_scan_analytics_table.sql
@@ -1,16 +1,16 @@
-CREATE TABLE admin.daily_analytics(
-    daily_analytics_id SERIAL PRIMARY KEY,
+CREATE TABLE admin.analytics(
+    analytics_id SERIAL PRIMARY KEY,
     average_user_visit TIME NOT NULL,
     museum_id INT NOT NULL,
-    CONSTRAINT fk_admin_daily_analytics_admin_museum FOREIGN KEY(museum_id) REFERENCES admin.museum(museum_id),
+    CONSTRAINT fk_admin_analytics_admin_museum FOREIGN KEY(museum_id) REFERENCES admin.museum(museum_id),
     date_created DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-CREATE INDEX idx_admin_daily_analytics_fk_museum_id
+CREATE INDEX idx_admin_analytics_fk_museum_id
 ON admin.museum(museum_id);
 
-CREATE INDEX idx_admin_daily_analytics_date_created
-ON admin.daily_analytics(date_created);
+CREATE INDEX idx_admin_analytics_date_created
+ON admin.analytics(date_created);
 
 
 CREATE TABLE admin.exhibit_analytics(
@@ -18,8 +18,8 @@ CREATE TABLE admin.exhibit_analytics(
     total_scans INT NOT NULL,
     exhibit_id INT NOT NULL,
     CONSTRAINT fk_admin_exhibit_analytics_exhibit_id FOREIGN KEY(exhibit_id) REFERENCES museum.exhibit(exhibit_id),
-    daily_analytics_id INT NOT NULL,
-    CONSTRAINT fk_admin_exhibit_analytics_admin_daily_analytics FOREIGN KEY(daily_analytics_id) REFERENCES admin.daily_analytics(daily_analytics_id),
+    analytics_id INT NOT NULL,
+    CONSTRAINT fk_admin_exhibit_analytics_admin_analytics FOREIGN KEY(analytics_id) REFERENCES admin.analytics(analytics_id),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -27,8 +27,8 @@ CREATE TABLE admin.exhibit_analytics(
 CREATE INDEX idx_admin_exhibit_analytics_fk_exhibit_id
 ON admin.exhibit_analytics(exhibit_id);
 
-CREATE INDEX idx_admin_exhibit_analytics_fk_daily_analytics_id
-ON admin.exhibit_analytics(daily_analytics_id);
+CREATE INDEX idx_admin_exhibit_analytics_fk_analytics_id
+ON admin.exhibit_analytics(analytics_id);
 
 CREATE TRIGGER tr_admin_exhibit_analytics_update_timestamp
     BEFORE UPDATE ON admin.exhibit_analytics
