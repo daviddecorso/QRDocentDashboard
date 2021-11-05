9547bb423daa33b373d9da6a0a4a11b703ec532f
Daniel Sanchez <sancd22@gmail.com>
2021-11-05 12:16:07 -0400

    indexed created_at museum scans

--- database/migrations/V1.9__added_table_scan.sql
+++ database/migrations/V1.9__added_table_scan.sql
@@ -14,6 +14,9 @@ ON museum.scan(user_id);
 CREATE INDEX idx_museum_scan_fk_exhibit_id
 ON museum.scan(exhibit_id);
 
+CREATE INDEX idx_museum_scan_created_at
+ON museum.scan(created_at);
+
 CREATE TRIGGER tr_museum_scan_update_timestamp
     BEFORE UPDATE ON museum.scan
     FOR EACH ROW
