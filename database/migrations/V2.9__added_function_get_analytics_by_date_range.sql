98f47e831f8589d84e5a3440fb0e528a0cdf1951
Daniel Sanchez <sancd22@gmail.com>
2021-11-06 19:40:45 -0400

    update .sql

--- database/migrations/V2.9__added_function_get_analytics_by_date_range.sql
+++ database/migrations/V2.9__added_function_get_analytics_by_date_range.sql
@@ -35,23 +35,6 @@ $$
         WHERE museum_id = _museum_id AND (date_created >= _start_date AND date_created <= _end_date)
         INTO _average_user_visit_from_range;
 
-        -- Represent the analytics and exhibit analytics in one query from given range (aggregate exhibits per one analytics day).
-        RETURN QUERY
-            SELECT COALESCE(_total_scans_from_range, 0), COALESCE(_average_daily_scans_from_range, 0), COALESCE(_average_user_visit_from_range, '00:00:00'::TIME),
-                   a.date_created AS date, SUM(ea.total_scans) AS total_scans, a.average_user_visit,
-                        json_agg(
-                            json_build_object(
-                                    'exhibitID', ea.exhibit_id,
-                                    'name', e.name,
-                                    'mainImage', e.image,
-                                    'scans', ea.total_scans
-                                )
-                            ) AS exhibit_analytics
-            FROM admin.analytics AS a
-                JOIN admin.exhibit_analytics AS ea ON a.analytics_id = ea.analytics_id
-                JOIN museum.exhibit AS e ON ea.exhibit_id = e.exhibit_id
-            WHERE a.museum_id = _museum_id AND (a.date_created >= _start_date AND a.date_created <= _end_date)
-            GROUP BY a.analytics_id, a.date_created
-            ORDER BY a.date_created DESC;
+        
     END
 $$
