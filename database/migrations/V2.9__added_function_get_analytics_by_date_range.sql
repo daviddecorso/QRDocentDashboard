16b0efb00c23c599c88ba32ce37bf53cc38415a2
Daniel Sanchez <sancd22@gmail.com>
2021-11-06 18:05:36 -0400

    update .sql

--- database/migrations/V2.9__added_function_get_analytics_by_date_range.sql
+++ database/migrations/V2.9__added_function_get_analytics_by_date_range.sql
@@ -3,13 +3,13 @@ CREATE OR REPLACE FUNCTION admin.fn_get_analytics_by_date_range(
    _start_date DATE = CAST(NOW() AT TIME ZONE 'EDT' AS DATE),
    _end_date DATE = CAST(NOW() AT TIME ZONE 'EDT' AS DATE)
 )
-RETURNS BOOLEAN
+RETURNS TABLE(_date DATE, _average_user_visit TIME, _total_scans INT, _average_daily_scans FLOAT, _exhibit_analytics json)
 LANGUAGE plpgsql
 AS
 $$
     DECLARE
         _total_scans INT;
-        _average_daily_scans INT;
+        _average_daily_scans FLOAT;
     BEGIN
         -- Get total scans from given range
         SELECT SUM(ea.total_scans) AS total_scans
@@ -28,21 +28,21 @@ $$
         INTO _average_daily_scans;
 
         -- Represent the analytics and exhibit analytics in one query from given range (aggregate exhibits per one analytics day).
-        SELECT a.date_created AS date, a.average_user_visit, COALESCE(_total_scans, 0), COALESCE(_average_daily_scans, 0),
-                    json_agg(
-                        json_build_object(
-                                'exhibitID', ea.exhibit_id,
-                                'name', e.name,
-                                'mainImage', e.image,
-                                'scans', ea.total_scans
-                            )
-                        ) AS exhibit_analytics
-        FROM admin.analytics AS a
-            JOIN admin.exhibit_analytics AS ea ON a.analytics_id = ea.analytics_id
-            JOIN museum.exhibit AS e ON ea.exhibit_id = e.exhibit_id
-        WHERE a.museum_id = _museum_id AND (a.date_created >= _start_date AND a.date_created <= _end_date)
-        GROUP BY a.analytics_id, a.date_created
-        ORDER BY a.date_created DESC;
-
+        RETURN QUERY
+            SELECT a.date_created AS date, a.average_user_visit, COALESCE(_total_scans, 0), COALESCE(ROUND(_average_daily_scans, 2), 0),
+                        json_agg(
+                            json_build_object(
+                                    'exhibitID', ea.exhibit_id,
+                                    'name', e.name,
+                                    'mainImage', e.image,
+                                    'scans', ea.total_scans
+                                )
+                            ) AS exhibit_analytics
+            FROM admin.analytics AS a
+                JOIN admin.exhibit_analytics AS ea ON a.analytics_id = ea.analytics_id
+                JOIN museum.exhibit AS e ON ea.exhibit_id = e.exhibit_id
+            WHERE a.museum_id = _museum_id AND (a.date_created >= _start_date AND a.date_created <= _end_date)
+            GROUP BY a.analytics_id, a.date_created
+            ORDER BY a.date_created DESC;
     END
 $$
