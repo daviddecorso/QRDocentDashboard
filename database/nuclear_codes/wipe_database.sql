266d780622f994007fd30ffe1cb2246fac2d66ac
Daniel Sanchez <sancd22@gmail.com>
2021-11-07 20:18:19 -0500

    update wipedatabase and api documentation

--- database/nuclear_codes/wipe_database.sql
+++ database/nuclear_codes/wipe_database.sql
@@ -1,8 +1,8 @@
 -- ************* DONT FORGET TO RE-ADD Tom's user back after wiping!!!!!!!!! *************
 DROP FUNCTION museum.fn_create_museum_scan(_user_id integer, _exhibit_id integer);
-DROP FUNCTION admin.fn_update_museum_exhibit(_exhibit_id integer, _name text, _description text, _exhibit_contents json);
+DROP FUNCTION admin.fn_update_museum_exhibit(_exhibit_id integer, _name text, _description text, _image text, _exhibit_contents json);
 DROP FUNCTION admin.fn_delete_museum_exhibit(_exhibit_id integer);
-DROP FUNCTION admin.fn_create_museum_exhibit(_name text, _description text, _exhibit_contents json, _exhibit_status_id integer, _museum_id integer);
+DROP FUNCTION admin.fn_create_museum_exhibit(_name text, _description text, _image text, _exhibit_contents json, _exhibit_status_id integer, _museum_id integer);
 DROP FUNCTION admin.fn_register_admin_user(_email TEXT, _password TEXT, _museum_id integer);
 DROP FUNCTION museum.fn_register_museum_user(_phone_number TEXT);
 DROP FUNCTION museum.fn_login_museum_user(_phone_number text, _confirmation_code text);
